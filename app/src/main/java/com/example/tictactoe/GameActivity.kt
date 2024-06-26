4b8daf1903bdc7ac240cdfb82e99015e93e84e25
hdunq11 <dunghoang080804@gmail.com>
2024-06-26 13:32:15 +0700

    update

---
+++ app/src/main/java/com/example/tictactoe/GameActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityGameBinding
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class GameActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityGameBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn0.setOnClickListener(this)
+        binding.btn1.setOnClickListener(this)
+        binding.btn2.setOnClickListener(this)
+        binding.btn3.setOnClickListener(this)
+        binding.btn4.setOnClickListener(this)
+        binding.btn5.setOnClickListener(this)
+        binding.btn6.setOnClickListener(this)
+        binding.btn7.setOnClickListener(this)
+        binding.btn8.setOnClickListener(this)
+        binding.startGameBtn.setOnClickListener {
+    startGame()
+        }
+    }
+    fun startGame(){
+
+    }
+
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
+
+}
---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.playOfflineBtn.setOnClickListener {
+            createOfflineGame()
+
+        }
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+    fun createOfflineGame(){
+    startGame()
+    }
+    fun startGame(){
+    startActivity(Intent(this,GameActivity::class.java))
+    }
+}
